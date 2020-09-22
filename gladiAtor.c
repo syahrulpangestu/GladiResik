c61ff3ed48a2bf1c2720522fc0352d9e727298b9
syahrulpangestu <syahrulpangestu11@gmail.com>
2020-09-22 21:09:15 +0700

    commit dua add "penjumlahan statis"

--- gladiAtor.c
+++ gladiAtor.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <conio.h>
 
 int main(){
-    printf("tes 1");
+    printf("tes 1 \n");
+    
+    int a = 5, b = 5, hasil;
+
+    hasil = a + b;
+    printf("%d",hasil);
+
 }
